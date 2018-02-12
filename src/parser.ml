0d8e0d4619e082192cf33d17ec15770854064765
3100 <3100.dev@gmail.com>
2018-02-13 02:58:41 +0900

    WIP (this does not pass tests)

--- src/parser.ml
+++ src/parser.ml
@@ -4,7 +4,14 @@ type command =
   | L_Command
   | Nothing;;
 
-(* 初期化 *)
+  (* HACK: too dificult for my future self to understand. Also this does not work. *)
+(* let is_c_command = Str.regexp "^\\([AMD]+=\\)?[\\+AMD01!|&-]+\\(;J[\\(GT|EQ|GE|LT|NE|LE|MP\\)]\\)?" *)
+(* HACK: not complete pattern. *)
+let _is_c_command = Str.regexp "^\\([AMD]+=\\)?\\([^()]+\\)\\(;J.+\\)?$"
+let _is_l_command = Str.regexp "^([a-zA-Z0-9]+)$" 
+
+let _trim_line line = String.trim line
+
 let line_stream_of_channel channel = 
   Stream.from
     (fun _ ->
@@ -15,32 +22,46 @@ let lines = Parser.line_stream_of_channel in_channel;
 Stream.next lines;
 *)
 
-let has_more_commands lines =
-  try Stream.empty(lines) != () with Stream.Failure -> true
-
-let command_type line =
-  let trimmed_line = String.trim line and 
-    (* HACK: too dificult for my future self to understand. Also this does not work. *)
-    (* is_c_command = Str.regexp "^\\([AMD]+=\\)?[\\+AMD01!|&-]+\\(;J[\\(GT|EQ|GE|LT|NE|LE|MP\\)]\\)?" and *)
-    (* HACK: not complete pattern. *)
-    is_c_command = Str.regexp "^\\([AMD]+=\\)?[^()]+\\(;J.+\\)?$" and 
-    is_l_command = Str.regexp "^([a-zA-Z0-9]+)$" in 
-    if String.get trimmed_line 0 == '@' then A_Command
-    else if Str.string_match is_c_command trimmed_line 0 then C_Command
-    else if Str.string_match is_l_command trimmed_line 0 then L_Command
-    else Nothing
-
 let advance lines =
   Stream.next lines
 
-(* below are for sample tests *)
+let command_type line =
+  let trimmed = _trim_line line in 
+  if String.get trimmed 0 == '@' then A_Command
+  else if Str.string_match _is_c_command trimmed 0 then C_Command
+  else if Str.string_match _is_l_command trimmed 0 then L_Command
+  else Nothing
 
-let say_hello msg = 
-  msg
+let has_more_commands lines =
+  try Stream.empty(lines) != () with Stream.Failure -> true
 
-let simple_check_123 str =
-  let regexp = Str.regexp ".*123.*" in
-  Str.string_match regexp str 0 
+(* Assume that this is only called when command_type is A_Command *)
+let symbol line = 
+  let trimmed = _trim_line line in 
+  String.sub trimmed 1 (String.length(trimmed) - 1)
 
-let a_command () =
-  A_Command
+(* Assume that this is only called when command_type is C_Command *)
+let dest line = 
+  let trimmed = _trim_line line in 
+  if String.contains trimmed '=' then
+    let len = String.index trimmed  '=' in
+    String.sub trimmed 0 (len - 1)
+  else ""
+
+(* Assume that this is only called when command_type is C_Command *)
+let jump line = 
+  let trimmed = _trim_line line in 
+  if String.contains trimmed ';' then
+    let si = (String.index trimmed ';') + 1 in 
+    let len = (String.length trimmed) - 1 - si in
+    String.sub trimmed si len 
+  else ""
+
+(* Assume that this is only called when command_type is C_Command *)
+let comp line =
+  let si = String.length (dest line) and
+  trimmed = _trim_line line in
+  let l = String.length trimmed and 
+  jumplen = String.length (jump line) in
+  let len = l - jumplen - 1 - si in
+  String.sub trimmed si len 
--- test/parser_test.ml
+++ test/parser_test.ml
@@ -1,9 +1,8 @@
 open OUnit2
 
-let assert_command str expected test_ctxt =
-  assert_equal (Parser.command_type str) expected
-
 let command_type_test =
+  let assert_command str expected test_ctxt =
+    assert_equal (Parser.command_type str) expected in
   "command type" >::: [
     "' @1' is A command" >:: assert_command " @1" A_Command;
     "'@123' is A command" >:: assert_command "@123" A_Command;
@@ -15,7 +14,33 @@ let command_type_test =
     "'(End)' is L Command" >:: assert_command "(End)" L_Command;
   ]
 
+let symbol_test =
+  let assert_symbol str expected test_ctxt =
+    assert_equal (Parser.symbol str) expected in
+  "symbol" >::: [
+    "@123 -> 123" >:: assert_symbol "@123" "123";
+    "@0 -> 0" >:: assert_symbol "@0" "0";
+  ]
+
+let dest_test = 
+  let assert_dest str expected test_ctxt =
+    assert_equal (Parser.dest str) expected in
+  "dest" >::: [
+    "0; JMP -> 0" >:: assert_dest "0; JMP" ""; (* In this case, dest is empty. *) 
+    "D = M - 1 -> D" >:: assert_dest "D = M - 1" "D";
+    "D - 1; JLE -> D - 1" >:: assert_dest "D - 1; JLE" "";
+  ]
+
+let comp_test =
+  let assert_dest str expected test_ctxt =
+    assert_equal (Parser.comp str) expected in
+  "comp" >::: [
+    "0; JMP -> 0" >:: assert_dest "0; JMP" "0"; (* In this case, comp is not empty. *) 
+    "D = M - 1 -> D" >:: assert_dest "D = M - 1" "M-1";
+    "D - 1; JLE -> D - 1" >:: assert_dest "D - 1; JLE" "D-1";
+  ]
+
 let tests =
   "all_tests" >::: [
-    command_type_test;
+    command_type_test; symbol_test; dest_test; comp_test;
   ]
