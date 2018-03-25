786de9884a6c69a5adb5a1eed1160c9453a3af02
3100 <3100.dev@gmail.com>
2018-03-25 23:12:55 +0900

    Remove unused functions from Parser.

--- test/parser_test.ml
+++ test/parser_test.ml
@@ -22,47 +22,6 @@ let parse_test =
     ""                          >:: assert_command ""   Nothing;
   ]
 
-let symbol_test =
-  let assert_symbol str expected test_ctxt =
-    assert_equal (Parser.symbol str) expected in
-  "symbol" >::: [
-    "@123 -> 123" >:: assert_symbol "@123" "123";
-    "@0 -> 0" >:: assert_symbol "@0" "0";
-    "(123) -> 123" >:: assert_symbol "(123)" "123";
-    "@HOGE -> HOGE" >:: assert_symbol "@HOGE" "HOGE";
-    "(END) -> END" >:: assert_symbol "(END)" "END";
-  ]
-
-let dest_test = 
-  let assert_dest str expected test_ctxt =
-    assert_equal (Parser.dest str) expected in
-  "dest" >::: [
-    "0; JMP     -> ''" >:: assert_dest "0; JMP"     ""; (* In this case, dest is empty. *) 
-    "D = M - 1  -> D"  >:: assert_dest "D = M - 1"  "D";
-    "D - 1; JLE -> ''" >:: assert_dest "D - 1; JLE" "";
-    "D;JGT      -> ''" >:: assert_dest "D;JGT"      "";
-  ]
-
-let comp_test =
-  let assert_comp str expected kest_ctxt =
-    assert_equal (Parser.comp str) expected in
-  "comp" >::: [
-    "0; JMP -> 0"         >:: assert_comp "0; JMP"     "0"; (* In this case, comp is not empty. *) 
-    "D = M - 1 -> D"      >:: assert_comp "D = M - 1"  "M-1";
-    "D - 1; JLE -> D - 1" >:: assert_comp "D - 1; JLE" "D-1";
-    "D;JGT"               >:: assert_comp "D;JGT"      "D";
-  ]
-
-let jump_test = 
-  let assert_jump str expected kest_ctxt =
-    assert_equal (Parser.jump str) expected in
-  "jump" >::: [
-    "0; JMP -> ''"      >:: assert_jump "0; JMP"     "JMP"; (* In this case, comp is not empty. *) 
-    "D = M - 1 -> ''"   >:: assert_jump "D = M - 1"  "";
-    "D - 1; JLE -> JLE" >:: assert_jump "D - 1; JLE" "JLE";
-    "D;JGT -> JGT"      >:: assert_jump "D;JGT"      "JGT";
-  ]
-
 let has_more_commands_test =
   let assert_has_more_commands lines kest_ctxt =
     assert_equal (Parser.has_more_commands lines) true in
@@ -88,10 +47,6 @@ let advance_test =
 let tests =
   "parser_tests" >::: [
     parse_test;
-    symbol_test;
-    dest_test;
-    comp_test;
-    jump_test;
     has_more_commands_test;
     advance_test;
   ]
